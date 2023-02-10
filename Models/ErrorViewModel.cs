2d1ccccbbe26ff789d2052d1b93afd4a68544ec6
Callum A <115150911+Carms001@users.noreply.github.com>
2023-02-10 14:20:47 +0000

    Part 2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IP3Group4Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
