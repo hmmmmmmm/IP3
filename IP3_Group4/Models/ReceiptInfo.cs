03f2c6a4486c3fadfa053864fdef17b7cb8d98b8
Alastair Kilgour <akilgour350@gmail.com>
2023-02-14 21:11:21 +0000

    no touchy

---
+++ IP3_Group4/Models/ReceiptInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IP3_Group4.Models
+{
+    public class ReceiptInfo
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Brand { get; set; }
+
+        public List<ProdLine> ProductLines { get; set; }
+    }
+
+    // TEMPORARY: delete and refactor when OrderLine class or equivalent is made
+    public class ProdLine
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public ProdLine(int iD, string product, int quantity, double price)
+        {
+            ID = iD;
+            Product = product;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
---
+++ IP3_Group4/Models/VisionClasses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IP3_Group4.Models
+{
+    public class MainRequests
+    {
+        public List<Request> requests { get; set; }
+    }
+
+    public class Request
+    {
+        public Img image { get; set; }
+        public List<Feature> features { get; set; }
+    }
+
+    public class Img
+    {
+        public string content { get; set; }
+    }
+    public class Feature
+    {
+        public string type { get; set; }
+    }
+}
