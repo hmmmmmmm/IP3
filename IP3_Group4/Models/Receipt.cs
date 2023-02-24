e82c3b27218ebefbcc331c79f1cfb9ca1a08ef73
Alastair Kilgour <akilgour350@gmail.com>
2023-02-24 12:15:53 +0000

    added comments to classes

--- IP3_Group4/Models/Receipt.cs
+++ IP3_Group4/Models/Receipt.cs
@@ -10,15 +10,16 @@ namespace IP3_Group4.Models
     public class Receipt
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ID { get; set; }
+        public int ID { get; set; } // Primary key for by database
 
         [Required]
-        public string Shop { get; set; }
-        public decimal TotalPrice { get; set; }
+        public string Shop { get; set; } // Shop where receipt was obtained
+        public decimal TotalPrice { get; set; } // TotalPrice of the receipt
 
         [DataType(DataType.DateTime), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm:ss}"), Required]
-        public DateTime PurchaseDate { get; set; }
+        public DateTime PurchaseDate { get; set; } // Date and Time of purchase
 
+        // Standard constructor
         public Receipt(string Shop, DateTime PurchaseDate)
         {
             this.Shop = Shop;
@@ -26,6 +27,7 @@ namespace IP3_Group4.Models
             ProductLines = new List<ProductLine>();
         }
 
+        // Empty constructor
         public Receipt()
         {
             this.ID = 0;
@@ -37,33 +39,39 @@ namespace IP3_Group4.Models
         }
 
         #region PaymentType Properties
+        // Used to store the PaymentType of the receipt.
         [Display(Name = "Payment Type"), ForeignKey("PaymentType")]
-        public int PaymentID { get; set; }
-        public PaymentType PaymentType { get; set; }
+        public int PaymentID { get; set; } // PaymentType ID
+        public PaymentType PaymentType { get; set; } // Actual payment type (cash, card)
         #endregion
 
         #region ProductLines Properties
+        // List of products found on the receipt
         public List<ProductLine> ProductLines { get; set; }
         #endregion
 
         #region User Properties
+        // Stores the user the receipt belongs to. For database purposes mostly.
         [Display(Name = "User"), ForeignKey("User")]
-        public string UserID { get; set; }
-        public User User { get; set; }
+        public string UserID { get; set; } // ID of the user
+        public User User { get; set; } // Actual user object
         #endregion
     }
 
+    // Stores different payment types
     public class PaymentType
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ID { get; set; }
-        public string Type { get; set; }
+        public int ID { get; set; } // PaymentType ID
+        public string Type { get; set; } // Actual type of payment (cash, card, etc.)
 
+        // standard constructor
         public PaymentType(string Type)
         {
             this.Type = Type;
         }
 
+        // empty constructor
         public PaymentType()
         {
             this.ID = 0;
@@ -76,15 +84,18 @@ namespace IP3_Group4.Models
         #endregion
     }
 
+    // Stores info on an actual product within a receipt
     public class ProductLine
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ID { get; set; }  
-        public string ItemName { get; set; }
-        public string Brand { get; set; }
-        public int Quantity { get; set; }
-        public decimal Price { get; set; }
-        public decimal LineTotal => (Price *= Quantity);
+        public int ID { get; set; } // ID of the ProductLine
+        public string ItemName { get; set; } // name of the Product (usually some kind of gibberish
+        public string Brand { get; set; } // the Brand of the product (eg. "Cadbury's")
+        public int Quantity { get; set; } // number of the product bought
+        public decimal Price { get; set; } // the price for each product
+        public decimal LineTotal => (Price *= Quantity); // the total price of Price * Quantity. automatically calculated
+
+        // standard constructor
         public ProductLine(int ID, string ItemName, int Quantity, decimal Price)
         {
             this.ID = ID;
@@ -93,6 +104,7 @@ namespace IP3_Group4.Models
             this.Price = Price;
         }
 
+        // emtpy constructor
         public ProductLine()
         {
             this.ID = 0;
@@ -103,9 +115,10 @@ namespace IP3_Group4.Models
         }
 
         #region Receipt Properties
+        // stores info on receipt its stored in (foreign key shennanigans)
         [Display(Name = "Receipt"), ForeignKey("Receipt")]
-        public int ReceiptID { get; set; }
-        public Receipt Receipt { get; set; }
+        public int ReceiptID { get; set; } // ID of the receipt
+        public Receipt Receipt { get; set; } // Actual receipt object
         #endregion
     }
 }
--- IP3_Group4/Models/ReceiptTemplate.cs
+++ IP3_Group4/Models/ReceiptTemplate.cs
@@ -8,25 +8,28 @@ using System.Web;
 namespace IP3_Group4.Models
 {
     // Class lets us create templates for the app to read in the receipts from
-
     public class ReceiptTemplate
     {
+
+        // ALWAYS STORE PROPERTIES IN LOWER CASE!!!!!!!!!!
+
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ID { get; set; }
+        public int ID { get; set; } // database primary key, no need to really touch
 
         [Required]
-        public string Shop { get; set; }
+        public string Shop { get; set; } // Stores the shop where this template can be used
 
-        public string ProductStartPrompt { get; set; }
+        public string ProductStartPrompt { get; set; } // The line ABOVE where the list of products are on the receipts
+                                                                             //▲ ▼ used to mark beginning and end of product list respectively  
+        public string ProductEndPrompt { get; set; } // The line BELOW where the list of products are on the receipts
 
-        public string ProductEndPrompt { get; set; }
+        public string QuantityLineFormat { get; set; } // Allows the format of quantity lines to be defined. (eg. on B&M has format "4 x 0.40" so this would be defined as " x ". Make sure to put in spaces
 
-        public string QuantityLineFormat { get; set; }
+        public string PaymentTypePrompt { get; set; } // The line above where the payment type is stored. On B&M: "PAID BY".
 
-        public string PaymentTypePrompt { get; set; }
+        public string DateTimePrompt { get; set; } // The line above where the Date and Time of purchase are stored on the receipt. Some receipts don't include it, so just pass "". Do not pass NULL.
 
-        public string DateTimePrompt { get; set; }
-
+        // Empty constructor. Only use for database purposes really.
         public ReceiptTemplate()
         {
             ID = 0;
@@ -38,6 +41,7 @@ namespace IP3_Group4.Models
             DateTimePrompt = "";
         }
 
+        // Standard constructor. Does what it says on the packet. Don't bother with setting ID tho as the database will do it for you.
         public ReceiptTemplate(string shop, string productStartPrompt, string productEndPrompt, string quantityLineFormat, string paymentTypePrompt, string dateTimePrompt)
         {
             Shop = shop;
